f6e677aa6049eace58356ec0d38151bbaad34c24
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2021-01-30 13:36:51 +0100

    improve tmux functions

--- functions.zsh
+++ functions.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+## misc convenience functions ##
 function ytdl {
     youtube-dl "$1" -x --audio-format mp3 --audio-quality 9
 }
@@ -20,9 +21,27 @@ function spip {
     fi
 }
 
+## Tmux automation ##
+function hsplit {
+    tmux new-session \; split-window -h
+}
+
+function vsplit {
+    tmux new-session \; split-window -v
+}
+
 function qsplit {
     # start four-way split tmux session
+    [[ "$1" != "" ]] && SESSNAME="$1" || SESSNAME="qsplit"
     tmux new-session \; split-window -h \; split-window -v \; select-pane -t 1 \; split-window -v \; select-pane -t 1 \; attach
+}
+
+function hexsplit {
+    # start six-way split tmux session
+    # maximize window if possible, makes no sense else
+    [[ "$1" != "" ]] && SESSNAME="$1" || SESSNAME="hexsplit"
+    wmctrl -r :ACTIVE: -b add,maximized_horz && wmctrl -r :ACTIVE: -b add,maximized_vert || true
+    tmux new-session -s "$SESSNAME"\; split-window -h -p 66 \; split-window -h -p 50 \; select-pane -t 1 \; split-window -v \; select-pane -t 3 \; split-window -v \; select-pane -t 5 \; split-window -v \; select-pane -t 1 \; attach
 }
 
 function pdot {
