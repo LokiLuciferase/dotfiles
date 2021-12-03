7b84adcbed6bed9e3baf6810d8fd6a46fa6dc4fc
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2021-12-03 13:08:35 +0100

    add script for migration to stowable configs

---
+++ scripts/migrations/to_v1.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+stow -h
+cd $HOME/.dotfiles && [ ! -d "$HOME/.dotfiles/scripts" ] && false
+
+for DIR in alacritty bumblebee-status dunst htop nvim picom i3 i3status ideavim tmux; do
+    mv "$HOME/.config/$DIR" "$HOME/.config/${DIR}.bak.d"
+    stow $DIR
+done
+
+mv $HOME/.config/tmux.bak.d/plugins $HOME/.config/tmux
+mv $HOME/.config/libinput-gestures.conf $HOME/.config/libinput-gestures.conf.bak
+stow libinput-gestures
