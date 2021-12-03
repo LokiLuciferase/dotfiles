c761dad186644354de54f13ce6ad79384feec900
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2021-12-03 13:18:20 +0100

    fix migration script

--- scripts/migrations/to_v1.sh
+++ scripts/migrations/to_v1.sh
@@ -5,7 +5,7 @@ stow -h
 cd $HOME/.dotfiles && [ ! -d "$HOME/.dotfiles/scripts" ] && false
 
 for DIR in alacritty bumblebee-status dunst htop nvim picom i3 i3status ideavim tmux; do
-    mv "$HOME/.config/$DIR" "$HOME/.config/${DIR}.bak.d"
+    mv "$HOME/.config/$DIR" "$HOME/.config/${DIR}.bak.d" || true
     stow $DIR
 done
 
