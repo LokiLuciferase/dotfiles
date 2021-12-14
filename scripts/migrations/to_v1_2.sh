163f5a604688e2816f08dd3e8311d8fb3255dcbc
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2021-12-14 10:17:28 +0100

    update migration script

--- scripts/migrations/to_v1_2.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rm ~/.zshrc && ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
--- scripts/run_latest_migration.sh
+++ scripts/run_latest_migration.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 LATEST_MIGRATION="$(ls -t ${SCRIPT_DIR}/migrations/ | head -1)"
 
+[ -z "${LATEST_MIGRATION}" ] && echo "No pending migrations found." && exit 0
+
 echo "Execute latest migration ($(basename $LATEST_MIGRATION))? [y/N]"
 read -r answer
 if [[ $answer =~ ^([yY][eE][sS]|[yY])$ ]]; then
