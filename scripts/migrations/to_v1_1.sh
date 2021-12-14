2af4c693bb248d0980582b1a3fcc2d11febf3a09
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2021-12-14 01:05:21 +0100

    add script to automatically apply the latest migration to dotfiles

--- scripts/migrations/to_v1_1.sh
+++
@@ -1,4 +0,0 @@
-set -euo pipefail
-
-rm $HOME/.SpaceVim.d || true
-ln -s $HOME/.dotfiles/spacevim/.SpaceVim.d $HOME/.SpaceVim.d
---
+++ scripts/run_latest_migration.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+LATEST_MIGRATION="$(ls -t ${SCRIPT_DIR}/migrations/ | head -1)"
+
+echo "Execute latest migration ($(basename $LATEST_MIGRATION))? [y/N]"
+read -r answer
+if [[ $answer =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    echo "Executing migration..."
+    bash "${SCRIPT_DIR}/migrations/${LATEST_MIGRATION}"
+else
+    echo "Skipping migration..."
+fi
