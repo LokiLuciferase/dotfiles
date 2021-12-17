12226c1596d8b23cec00163ebb34d52aeb1d3e61
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2021-12-17 11:31:39 +0100

    fix histfile migration

--- scripts/migrations/1639734628_move_histfile.sh
+++ scripts/migrations/1639734628_move_histfile.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # SUMMARY: move zsh history file to XDG directory
 ZSH_CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/zsh"
 mkdir -p "$ZSH_CACHE"
-mv "$HOME/.zsh_history" "$ZSH_CACHE"
+mv "$HOME/.zsh_history" "$ZSH_CACHE/history"
