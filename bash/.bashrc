b07b8a3dca69c724457da7c9b0746cb8df1c8b24
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2021-12-20 00:42:33 +0100

    fix overwriting of zsh history by bash history

---
+++ bash/.bashrc
@@ -0,0 +1,8 @@
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+BASHCACHE="${XDG_CACHE_HOME:-~/.cache}/bash"
+mkdir -p "$BASHCACHE"
+export HISTFILE="$BASHCACHE/bash_history"
--- zsh/env_vars.zsh
+++ zsh/env_vars.zsh
@@ -1,4 +1,8 @@
 # bookmarks support
+if [ ! -z "$BASH" ]; then
+    echo "Sourcing environment in bash. This may yield unexpected results."
+fi
+
 if [ -d "${HOME}/.local/share/bookmarks" ]; then
     export CDPATH=".:${HOME}/.local/share/bookmarks"
 fi
@@ -14,6 +18,8 @@ export XDG_CACHE_HOME=$HOME/.cache
 # retarget misc tools to XDG
 export SSB_HOME="$XDG_DATA_HOME"/zoom
 export CONDARC="$XDG_CONFIG_HOME/conda/condarc"
+export IPYTHONDIR="$XDG_CONFIG_HOME/ipython"
+export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
 export BASH_COMPLETION_USER_FILE="$XDG_CONFIG_HOME"/bash-completion/bash_completion
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export CRAWL_DIR="$XDG_DATA_HOME"/crawl/
@@ -24,7 +30,9 @@ export GTK_RC_FILES="$XDG_CONFIG_HOME"/gtk-1.0/gtkrc
 export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
 export IPFS_PATH="$XDG_DATA_HOME"/ipfs
 export MYSQL_HISTFILE="$XDG_DATA_HOME"/mysql_history
-export HISTFILE="${XDG_CACHE_HOME}/zsh/history"  # do not source with bash! overwrites zsh history
+export HISTSIZE=1000000  # disable truncation of history when starting bash
+export HISTFILESIZE=1000000
+[ -z "$BASH" ] && export HISTFILE="${XDG_CACHE_HOME}/zsh/history"  # do not source with bash! overwrites zsh history
 export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
 export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
 export TERMINFO="${XDG_DATA_HOME}/terminfo"
