84fd37acc652b294576cfdd1fad38fe6e61f159a
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2022-07-31 12:33:26 +0200

    move conda init function to regular functions.zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,22 @@
 #!/usr/bin/env zsh
 
+
+conda-init() {
+    # initialize conda environment
+    local conda_basedir=${1:-${HOME}/miniconda3}
+    __conda_setup="$("${conda_basedir}/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "${conda_basedir}/etc/profile.d/conda.sh" ]; then
+            . "${conda_basedir}/etc/profile.d/conda.sh"
+        else
+            export PATH="${conda_basedir}/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+}
+
 ## misc convenience functions ##
 ytdl-mp3() {
     yt-dlp "$1" -x --audio-format mp3 --audio-quality 9
