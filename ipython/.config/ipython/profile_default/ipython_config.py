96016937a0840b41db0645db64be0c52d1a43320
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2022-08-05 08:37:03 +0200

    ingest ipython config file

---
+++ ipython/.config/ipython/profile_default/ipython_config.py
@@ -0,0 +1,5 @@
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.InteractiveShell.colors = 'Linux'
+c.InteractiveShell.color_info = True
+c.TerminalInteractiveShell.highlighting_style = 'monokai'
+c.TerminalInteractiveShell.highlight_matching_brackets = True
