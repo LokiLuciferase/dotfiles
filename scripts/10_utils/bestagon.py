8d864ec59a4d94802d1d16be7ab0d03b0c6d28ad
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2022-08-20 13:11:49 +0200

    add iterative mode to bestagon.py

--- scripts/10_utils/bestagon.py
+++ scripts/10_utils/bestagon.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
+from typing import Optional
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from pathlib import Path
 
 import numpy as np
 from PIL import Image
+from tqdm.auto import tqdm
 from tqdm.contrib.concurrent import process_map
 
 try:
     from numba import jit
 except ImportError as e:
     print('Cannot import numba - bestagonization will be slow.')
+
     def jit(f):
         def wrapper(*args, **kwargs):
-            return f(*args, **kwargs) 
+            return f(*args, **kwargs)
+
         return wrapper
 
 
@@ -30,7 +34,18 @@ def get_args():
     parser.add_argument('-i', '--input', required=True, help='The file to process')
     parser.add_argument('-o', '--output', help='The name of the output file.')
     parser.add_argument(
-        '-c', '--count', default=32, type=int, help='The number of bestagons per row.'
+        '-c',
+        '--count',
+        default=32,
+        type=int,
+        nargs='+',
+        help='The number of bestagons per row. If multiple are passed, create an image for each.',
+    )
+    parser.add_argument(
+        '-r',
+        '--rescale_height',
+        default=None,
+        help='The height (in px) to rescale the image to before applying bestagon filter.',
     )
     parser.add_argument(
         '--spiky',
@@ -39,10 +54,10 @@ def get_args():
         help='Whether to have the spiky side of the bestagon on top.',
     )
     parser.add_argument(
-        '-r',
-        '--rescale_height',
-        default=None,
-        help='The height (in px) to rescale the image to before applying bestagon filter.',
+        '--iterative',
+        action='store_true',
+        default=False,
+        help='Whether to perform all operations iteratively on the same image.',
     )
     args = parser.parse_args()
     return args
@@ -63,9 +78,7 @@ def hexagon_map(row: int, col: int, resolution: np.ndarray, count: int, r: np.nd
     uv = uv * count * RATIO
     z = min(max(abs(((uv[0] + np.floor(uv[1])) % 2.0) - 1.0) * PI - CONST_A, 0.0), 1.0)
     uv[1] = np.floor(uv[1] + z)
-    uv[0] = (
-        np.floor(uv[0] * 0.5 + (uv[1] % 2.0) * 0.5) - (uv[1] % 2.0) * 0.5 + 0.5
-    ) * CONST_B
+    uv[0] = (np.floor(uv[0] * 0.5 + (uv[1] % 2.0) * 0.5) - (uv[1] % 2.0) * 0.5 + 0.5) * CONST_B
     new_row, new_col = resolution * (uv / count * r)
     return min(int(new_row), resolution[0] - 1), min(int(new_col), resolution[1] - 1)
 
@@ -80,6 +93,7 @@ def parallelize_over_rows(tup):
 
 
 def convert(inp: str, outp: str, count: int, flat: bool, rescale_height: int):
+    origcount = count
     count = count * 2
     pic = Image.open(inp)
     if rescale_height is not None:
@@ -98,7 +112,13 @@ def convert(inp: str, outp: str, count: int, flat: bool, rescale_height: int):
     r = np.array([resolution[1] / resolution[0], 1.0])
 
     tups = [(row, resolution, count, r) for row in range(resolution[0])]
-    mapped = process_map(parallelize_over_rows, tups, chunksize=1)
+    mapped = process_map(
+        parallelize_over_rows,
+        tups,
+        chunksize=1,
+        desc=f'count={origcount}{" (spiky)" if not flat else ""}',
+        leave=False,
+    )
     for row, mapping in enumerate(mapped):
         for col, (mapped_row, mapped_col) in enumerate(mapping):
             newpix[row, col] = pix[mapped_row, mapped_col]
@@ -109,31 +129,36 @@ def convert(inp: str, outp: str, count: int, flat: bool, rescale_height: int):
     im.save(outp)
 
 
-if __name__ == '__main__':
-    args = get_args()
-
-    if args.output is None:
-        fin = Path(args.input)
-        fout = fin.parent / f'{fin.stem}_bg{args.count}{"s" if args.spiky else ""}{fin.suffix}'
-        if fout.is_file():
+def format_outp(inp: Path, outp: Optional[Path], count: int, spiky: bool, iterative: bool):
+    if iterative:
+        count = ''  # write to same file
+    if outp is None:
+        fin = Path(inp)
+        fout = fin.parent / f'{fin.stem}_bg{count}{"s" if spiky else ""}{fin.suffix}'
+        if fout.is_file() and not iterative:
             raise RuntimeError(f'Default output file already exists: {fout}')
-        args.output = str(fout)
+        outp = str(fout)
+    return outp
 
-    if args.rescale_height is not None:
+
+def main(inp: Path, outp: Path, count: int, spiky: bool, rescale_height: str, iterative: bool):
+    outp = format_outp(inp, outp, count, spiky, iterative)
+    if rescale_height is not None:
         rh = {
             '8k': 4320,
             '4k': 2160,
             'qhd': 1440,
             'fhd': 1080,
-        }.get(args.rescale_height.lower(), args.rescale_height)
+        }.get(rescale_height.lower(), rescale_height)
         rh = int(rh)
     else:
         rh = None
 
-    convert(
-        args.input,
-        args.output,
-        int(args.count),
-        flat=(not args.spiky),
-        rescale_height=rh,
-    )
+    convert(inp, outp, count, flat=(not args.spiky), rescale_height=rh)
+
+
+if __name__ == '__main__':
+    args = get_args()
+    counts = args.count if len(args.count) == 1 else tqdm(args.count, desc='Converting')
+    for count in counts:
+        main(args.input, args.output, count, args.spiky, args.rescale_height, args.iterative)
