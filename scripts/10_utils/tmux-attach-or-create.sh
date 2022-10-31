75ffaa7ff3c3e5b2b0807cb9cf8be20cbb364844
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2022-10-31 16:53:48 +0100

    refine tmux attach script

--- scripts/10_utils/tmux-attach-or-create.sh
+++ scripts/10_utils/tmux-attach-or-create.sh
@@ -4,22 +4,18 @@ set -euo pipefail
 
 TMUX_CONFIG="$HOME/.config/tmux/tmux.conf"
 
-
-get-latest-numbered-tmux-session() {
-    local sessname
-    sessname=$(tmux list-sessions | grep -oE '[0-9]+$' | sort -n | tail -n 1)
-    if [[ -z "$sessname" ]]; then
-        echo "0"
-    else
-        echo "$sessname"
-    fi
-    return 0
-}
-
 if [[ $# -eq 0 ]]; then
-    SESS_TO_ATTACH=$(get-latest-numbered-tmux-session)
+    ATTACH_NAME=$(tmux list-sessions | grep -oE '[0-9]+$' | sort -n | tail -n 1 || true)
 else
-    SESS_TO_ATTACH=$1
+    ATTACH_NAME=$1
 fi
 
-tmux -f "${TMUX_CONFIG}" attach -t "$SESS_TO_ATTACH" || tmux -f "${TMUX_CONFIG}" new -s "$SESS_TO_ATTACH"
+if [[ -z "$ATTACH_NAME" ]]; then
+    NEW_SESS_ARG=""
+else
+    NEW_SESS_ARG="-s $ATTACH_NAME"
+fi
+
+CMD="tmux -f ${TMUX_CONFIG} attach -t $ATTACH_NAME || tmux -f ${TMUX_CONFIG} new $NEW_SESS_ARG"
+echo "${CMD}"
+eval $CMD
