e580e82265e54fb3088c88b7ced9f2a4125bded0
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2022-10-31 17:04:14 +0100

    refine tmux attach script again

--- scripts/10_utils/tmux-attach-or-create.sh
+++ scripts/10_utils/tmux-attach-or-create.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# If there is a numbered tmux session (not representing a persistent job), attach to it. Otherwise, create a new one.
 
 TMUX_CONFIG="$HOME/.config/tmux/tmux.conf"
 
 if [[ $# -eq 0 ]]; then
-    ATTACH_NAME=$(tmux list-sessions | grep -oE '[0-9]+$' | sort -n | tail -n 1 || true)
+    ATTACH_NAME=$(tmux list-sessions -F '#{session_name}' | grep -oE '[0-9]+$' | sort -n | tail -n 1 || true)
 else
     ATTACH_NAME=$1
 fi
