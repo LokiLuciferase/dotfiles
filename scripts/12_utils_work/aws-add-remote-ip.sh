e9eeea5d6a1281437e8271b80c46809df05ad4c4
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2023-03-21 11:59:38 +0100

    target master branch of datafiles and update aws add remote ip script to allow passing of IP to add

--- scripts/12_utils_work/aws-add-remote-ip.sh
+++ scripts/12_utils_work/aws-add-remote-ip.sh
@@ -2,17 +2,16 @@
 set -euo pipefail
 
 ############### Global Variables ##########
-SCRIPT_PATH="`dirname \"$0\"`"
 INGRESS_PORT=22
 ACCOUNT_NAME=rnd
 AWSCLI_PROFILE=rnd
 SECURITY_GROUP_ID="$1"
+PUBLIC_IP="${2:-$(curl ifconfig.me)}"
 ###########################################
 
 maybe_aws_sso_login() {
     set +e
-    aws s3 --profile ${AWSCLI_PROFILE} ls &> /dev/null
-    [ "$?" -eq 0 ] && return
+    aws s3 --profile ${AWSCLI_PROFILE} ls &> /dev/null && return
     echo "Need to login to AWS SSO first."
     aws sso login
     set -e
@@ -21,16 +20,16 @@ maybe_aws_sso_login() {
 purge_existing_rules() {
     echo "Loading Security Group information..."
     local rules_to_revoke
-    rules_to_revoke=$(aws --profile $AWSCLI_PROFILE ec2 describe-security-groups --group-ids $SECURITY_GROUP_ID --output text | grep IPRANGES | grep -v "0.0.0.0" | grep -v "KEEP" | cut -f2 | tr '\n' ' ')
+    rules_to_revoke=$(aws --profile $AWSCLI_PROFILE ec2 describe-security-groups --group-ids "$SECURITY_GROUP_ID" --output text | grep IPRANGES | grep -v "0.0.0.0" | grep -v "KEEP" | cut -f2 | tr '\n' ' ')
     for rule in ${rules_to_revoke[@]}; do
         echo "Revoking ingress rule $rule..."
-        AWS_PAGER="" aws --profile $AWSCLI_PROFILE ec2 revoke-security-group-ingress --group-id $SECURITY_GROUP_ID --cidr $rule --protocol tcp --port $INGRESS_PORT
+        AWS_PAGER="" aws --profile $AWSCLI_PROFILE ec2 revoke-security-group-ingress --group-id "$SECURITY_GROUP_ID" --cidr "$rule" --protocol tcp --port $INGRESS_PORT
     done
 }
 
 add_current_ip() {
     AWS_PAGER="" aws --profile $AWSCLI_PROFILE ec2 authorize-security-group-ingress \
-      --group-id $SECURITY_GROUP_ID \
+      --group-id "$SECURITY_GROUP_ID" \
       --protocol tcp \
       --port $INGRESS_PORT \
       --cidr $1/32
@@ -39,17 +38,15 @@ add_current_ip() {
 main() {
     maybe_aws_sso_login
     echo -n "Purge obsolete ingress rules? [y/N] "
-    read yn_purge
+    read -r yn_purge
     if [[ "$yn_purge" = "y" ]]; then
         purge_existing_rules
     fi
 
-    local public_ip
-    public_ip=$(curl -s ifconfig.me)
-    echo -n "Add the current public IP ($public_ip) to your DMZ security group in account ${ACCOUNT_NAME}? [y/N] "
-    read yn_add
+    echo -n "Add the current public IP (${PUBLIC_IP}) to your DMZ security group in account ${ACCOUNT_NAME}? [y/N] "
+    read -r yn_add
     if [[ "$yn_add" = "y" ]]; then
-        add_current_ip $public_ip
+        add_current_ip "${PUBLIC_IP}"
     fi
 }
 
