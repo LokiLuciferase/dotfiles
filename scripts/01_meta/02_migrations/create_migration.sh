d37e0f302742ea431066a521be654f050475b57d
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2023-04-29 17:45:10 +0200

    add generated code to run dotfiles migration only in desktop environments

--- scripts/01_meta/02_migrations/create_migration.sh
+++ scripts/01_meta/02_migrations/create_migration.sh
@@ -5,10 +5,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TIMESTAMP="$(date +%s)"
 
 echo -n "FILENAME_SUFFIX: "
-read FILENAME_SUFFIX
+read -r FILENAME_SUFFIX
 
 echo -n "SUMMARY: "
-read SUMMARY
+read -r SUMMARY
+
+echo -n "Desktop only? [y/N]: "
+read -r DESKTOP_ONLY
 
 FILENAME="${SCRIPT_DIR}/migrations/${TIMESTAMP}_${FILENAME_SUFFIX}.sh"
 
@@ -16,10 +19,23 @@ cat << EOF > "${FILENAME}"
 #!/usr/bin/env bash
 set -euo pipefail
 
-# FILENAME_SUFFIX: $FILENAME_SUFFIX
 # SUMMARY: $SUMMARY
+# ISSUED: $(date --rfc-3339=seconds)
 # ---
+EOF
+
+if [[ "${DESKTOP_ONLY}" =~ ^[Yy]$ ]]; then
+    cat << 'EOF' >> "${FILENAME}"
+if [ -z "$(ls -A ~/.dotfiles/datafiles/ || true)" ]; then
+    echo "Skipping desktop-only migration."
+    exit 0
+else
+    echo "Running desktop-only migration."
+fi
 
 EOF
+else
+    echo >> "${FILENAME}"
+fi
 
 nvim + "${FILENAME}"
