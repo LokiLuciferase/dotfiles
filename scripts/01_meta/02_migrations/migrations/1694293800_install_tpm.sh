5a5d4448e72f516f2b0f0e94462db04962ce8760
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2023-09-09 23:32:56 +0200

    add Tmux plugin manager, take 2

---
+++ scripts/01_meta/02_migrations/migrations/1694293800_install_tpm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# SUMMARY: Installs Tmux Plugin Manager and plugins
+# ISSUED: 2023-09-09 23:10:21+02:00
+# ---
+if [ ! -d ~/.config/tmux ]; then
+    echo "Tmux config directory not found. Exiting."
+    exit 1
+elif [ -d ~/.config/tmux/plugins/tpm ] || [ -L ~/.config/tmux/plugins/tpm ]; then
+    # tpm is already installed
+    exit 0
+elif [ ! -d ~/.config/tmux/plugins ]; then
+    mkdir -p ~/.config/tmux/plugins
+fi
+
+# Install tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+
+# Install plugins
+tmux new-session 'tmux source ~/.config/tmux/tmux.conf && ~/.config/tmux/plugins/tpm/bin/install_plugins'
