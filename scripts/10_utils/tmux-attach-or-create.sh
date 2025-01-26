edf36e4cde898c865fee96fcb68144cb69c77baa
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2025-01-26 16:31:43 +0100

    fix and simplify/refactor tmux attach script

--- scripts/10_utils/tmux-attach-or-create.sh
+++ scripts/10_utils/tmux-attach-or-create.sh
@@ -2,20 +2,27 @@
 set -euo pipefail
 
 # If there is a numbered tmux session (not representing a persistent job) which is not currently attached to, attach to it. Otherwise, create a new one.
-
 TMUX_CONFIG="$HOME/.config/tmux/tmux.conf"
 
-if [[ $# -eq 0 ]]; then
-    ATTACH_NAME=$(tmux list-sessions -F '#{session_name}#{?session_attached,--attached,}' | grep -v -- '--attached' 2> /dev/null | grep -oE '[0-9]+$' 2> /dev/null | sort -n | tail -n 1 || true)
-else
-    ATTACH_NAME=$1
-fi
+get-attach-name() {
+    tmux list-sessions -F '#{session_name}#{?session_attached,--attached,}' \
+        | grep -v -- '--attached' 2> /dev/null \
+        | grep -oE '[0-9]+$' 2> /dev/null \
+        | sort -n \
+        | tail -n 1 \
+        || return 0
+}
 
-if [[ -z "$ATTACH_NAME" ]]; then
-    NEW_SESS_ARG=""
-else
-    NEW_SESS_ARG="-s $ATTACH_NAME"
-fi
+main() {
+    local attach_name
+    local cmd
+    attach_name=$(get-attach-name)
+    if [[ ! -z "${attach_name}" ]]; then
+        cmd="tmux -f ${TMUX_CONFIG} attach -t $attach_name"
+    else
+        cmd="tmux -f ${TMUX_CONFIG} new"
+    fi
+    eval $cmd
+}
 
-CMD="tmux -f ${TMUX_CONFIG} attach -t $ATTACH_NAME &> /dev/null || tmux -f ${TMUX_CONFIG} new $NEW_SESS_ARG"
-eval $CMD
+main
