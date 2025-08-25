24ecc3a14df482b2ba4184a1062e1a1ac45632ee
Lukas Lüftinger <llf@aitiologic.com>
2025-08-25 17:36:05 +0200

    switch nvim to catppuccin

--- nvim/.config/nvim/02_plugins.lua
+++ nvim/.config/nvim/02_plugins.lua
@@ -20,18 +20,24 @@ end
 
 -- define plugin spec
 local plugin_spec = {
+    -- {
+    --     'sainnhe/sonokai',
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         -- Optionally configure and load the colorscheme
+    --         -- directly inside the plugin declaration.
+    --         vim.g.sonokai_enable_italic = true
+    --         vim.g.sonokai_style = 'atlantis'
+    --         vim.g.sonokai_better_performance = true
+    --         vim.cmd.colorscheme('sonokai')
+    --     end
+    -- },
     {
-        'sainnhe/sonokai',
-        lazy = false,
-        priority = 1000,
+        "catppuccin/nvim",
         config = function()
-            -- Optionally configure and load the colorscheme
-            -- directly inside the plugin declaration.
-            vim.g.sonokai_enable_italic = true
-            vim.g.sonokai_style = 'atlantis'
-            vim.g.sonokai_better_performance = true
-            vim.cmd.colorscheme('sonokai')
-        end
+            vim.cmd.colorscheme("catppuccin-macchiato")
+        end,
     },
     {
         -- Syntax highlighting for NF
