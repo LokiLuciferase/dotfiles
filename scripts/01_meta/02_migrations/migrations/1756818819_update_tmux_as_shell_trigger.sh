72ea0094269f1690eed920138f63d85a4a80b15e
Lukas Lüftinger <lukas.lueftinger@outlook.com>
2025-09-02 15:20:07 +0200

    fix migration

--- scripts/01_meta/02_migrations/migrations/1756818819_update_tmux_as_shell_trigger.sh
+++ scripts/01_meta/02_migrations/migrations/1756818819_update_tmux_as_shell_trigger.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 # SUMMARY: For environments where USE_TMUX_AS_SHELL=true, create the file ~/.local/state/tmux/use_as_shell, which serves as the new trigger for starting directly into tmux when requested.
 # ISSUED: 2025-09-02 15:14:44+02:00
 # ---
+source ~/.dotfiles/zsh/.config/zsh/local/env-vars.local.zsh || exit 0
 if [ "${USE_TMUX_AS_SHELL:-}" = "true" ]; then
     mkdir -p ~/.local/state/tmux
     touch ~/.local/state/tmux/use_as_shell
