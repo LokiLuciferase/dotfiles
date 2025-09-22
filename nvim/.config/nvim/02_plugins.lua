29ffbc036a4885ad7eafff1d6dbd60947358cf61
Lukas Lüftinger <llf@aitiologic.com>
2025-09-22 17:06:47 +0200

    disable treesitter highlighting for latex

--- nvim/.config/nvim/02_plugins.lua
+++ nvim/.config/nvim/02_plugins.lua
@@ -316,7 +316,11 @@ local plugin_spec = {
                     "dockerfile",
                     "lua", "vim"
                 },
-                highlight = { enable = true, additional_vim_regex_highlighting = false },
+                highlight = {
+                    disable = {"latex"},
+                    enable = true,
+                    additional_vim_regex_highlighting = false
+                },
                 indent = { enable = true },
             })
         end
